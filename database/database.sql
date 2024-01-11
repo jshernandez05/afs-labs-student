d11fbd092217d8623380efdbbacb4f5f71689201
jshernandez05 <jshernandez05@gmail.com>
2024-01-11 17:27:05 +0000

    added comment to database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,4 +1,4 @@
---
+-- Comment from Lab 6
 -- PostgreSQL database dump
 --
 
@@ -14,14 +14,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
